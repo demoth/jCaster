8bfee5f9d70b4b213b0ecee8a3cf64f43fe8f6cf
Daniil <bubnov.d.e@gmail.com>
2013-04-08 22:14:55 +0400

    added caster protocol

---
+++ src/org/caster/client/CasterJSONProtocol.java
@@ -0,0 +1,66 @@
+package org.caster.client;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: daniil
+ * Date: 08.04.13
+ * Time: 21:47
+ * To change this template use File | Settings | File Templates.
+ */
+public class CasterJSONProtocol implements CasterProtocol{
+
+    @Override
+    public void chat(String message) {
+        //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    @Override
+    public void login(String username, String password) {
+        //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    @Override
+    public void join(String charId) {
+        //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    @Override
+    public void createCharacter(String properties) {
+        //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    @Override
+    public void action(String type, String... details) {
+        //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    @Override
+    public void disconnect() {
+        //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    @Override
+    public void showInventory() {
+        //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    @Override
+    public void showMap() {
+        //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    @Override
+    public void showSkills() {
+        //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    @Override
+    public void showStats() {
+        //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    @Override
+    public void processMessage(String message, GameData data) {
+        //To change body of implemented methods use File | Settings | File Templates.
+    }
+}
---
+++ src/org/caster/client/CasterProtocol.java
@@ -0,0 +1,38 @@
+package org.caster.client;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: daniil
+ * Date: 08.04.13
+ * Time: 21:48
+ */
+public interface CasterProtocol {
+
+    ////////////////////
+    // From client
+    ////////////////////
+    public void chat(String message);
+
+    public void login(String username, String password);
+
+    public void join(String charId);
+
+    public void createCharacter(String properties);
+
+    public void action(String type, String... details);
+
+    public void disconnect();
+
+    public void showInventory();
+
+    public void showMap();
+
+    public void showSkills();
+
+    public void showStats();
+
+    ////////////////////
+    // From server
+    ////////////////////
+    public void processMessage(String message, GameData data);
+}
